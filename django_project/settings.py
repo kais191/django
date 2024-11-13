bfee1bfef5e0dbbd769f23144befa691955db869
kais ben sadek <dasy.homar@gmail.com>
2024-11-13 22:00:29 +0000

    Update settings.py to use default SECRET_KEY for testing

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY","rdAkWzbkeRg-I5uFOxpyg3m7wKnrjbqyorz58VEbs0ZZTHz0D9ovXrZ7ggCxjKY0Tm4")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'True'
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
+
 
 # Application definition
 
@@ -76,8 +78,6 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-database_url = os.environ.get("DATABASE_URL")
 
 DATABASES = {
     'default': dj_database_url.config(
