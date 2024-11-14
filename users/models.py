799304b4fc8651a1668e28feed8df8bcfcb8baf7
kais ben sadek <dasy.homar@gmail.com>
2024-11-14 03:33:54 +0000

    Updated models and applied migrations

--- users/models.py
+++ users/models.py
@@ -4,22 +4,18 @@ from PIL import Image  # To handle image resizing (optional)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='profile_pics/default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
     def save(self, *args, **kwargs):
-        """
-        Override the save method to ensure a default image path and resize profile images, if necessary.
-        """
-        # Set a default image path if none is provided
+        # Use the default image if no image is provided
         if not self.image:
-            self.image = 'profile_pics/default.jpg'  # Adjust this path as needed
-
-        super().save(*args, **kwargs)  # Save the profile instance first
+            self.image = 'profile_pics/default.jpg'
+        super().save(*args, **kwargs)
         
-        # Optional resizing of the image
+        # Resize image if needed
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
