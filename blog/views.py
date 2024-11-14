6d2f078f80c314a2e804f8502b93f29384dd018f
kais ben sadek <dasy.homar@gmail.com>
2024-11-14 18:20:34 +0000

    Cleaned up views, removed duplicates, and added login check for toggle_favorite

--- blog/views.py
+++ blog/views.py
@@ -1,107 +1,37 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 import json
 import random
-from .models import Post, Announcement, Event 
-from .models import Post
-from django.shortcuts import get_object_or_404, redirect
 
+from .models import Post, Announcement, Event
 
-
-
-
-
-def toggle_favorite(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    if request.user in post.favorites.all():
-        post.favorites.remove(request.user)
-    else:
-        post.favorites.add(request.user)
-    return redirect('blog-home')  # Or wherever you want to redirect
-
-
-
-
-
-def about_view(request):
-    latest_posts = Post.objects.order_by('-date_posted')[:5]  # Get the latest 5 posts
-    announcements = Announcement.objects.all()[:5]  # Get the latest 5 announcements
-    calendar_events = Event.objects.all()[:5]  # Get the latest 5 events
-    
-    context = {
-        'latest_posts': latest_posts,
-        'announcements': announcements,
-        'calendar_events': calendar_events,
-    }
-    
-    return render(request, 'blog/about.html', context)
-
-
-def delete_message(request):
-    if request.method == "POST":
-        # Retrieve and process the message ID or other data sent from the frontend
-        data = json.loads(request.body)
-        message_id = data.get('messageId')
-        
-        # Perform deletion or logging actions here, if needed
-        # For example, deleting from a database if you store messages
-
-        return JsonResponse({"status": "success", "message": "Message deleted."})
-    return JsonResponse({"status": "error", "message": "Invalid request."})
-
-# Example auto-responses
+# Automatic responses for chat bot
 AUTO_RESPONSES = [
     "Hello! How can I help you?",
     "I’m here to assist you with any questions.",
     "Feel free to ask anything!",
     "I'm just an auto-responder, but I'll try my best!",
-    
 ]
 
-def about_view(request):
-    return render(request, 'blog/about.html')
-
-def chat_response(request):
-    if request.method == "POST":
-        user_message = request.POST.get("message")
-        bot_response = random.choice(AUTO_RESPONSES)
-        return JsonResponse({"user_message": user_message, "bot_response": bot_response})
-
-
-
-
-
-
-from django.views.generic import (
-    ListView, 
-    DetailView, 
-    CreateView,
-    UpdateView,
-    DeleteView # New import added here
-)
-from django.http import HttpResponse
-from .models import Post
-
-# Create your views here.x
 def home(request):
     context = {
        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
-class PostlistView(ListView):
+class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
+    template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted'] 
     paginate_by = 5
 
-
 class PostDetailView(DetailView):
     model = Post  
-    
+
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
@@ -120,36 +50,53 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     
     def test_func(self):
         post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
+        return self.request.user == post.author
     
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView): # New class PostDeleteView created here
-    model = Post
-    
-    def test_func(self):
-        post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
-
-def about(request):
-    return render(request, 'blog/about.html', {'title': 'About'})
-
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    success_url = "/" # Here we are redirecting the user back to the homepage after deleting a Post successfully
+    success_url = "/"
     
     def test_func(self):
         post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
-    
-    
+        return self.request.user == post.author
 
 class UserPostListView(ListView):
     model = Post
-    template_name = 'blog/user_posts.html' # ‹app>/<model >_<viewtype>.html
+    template_name = 'blog/user_posts.html'
     context_object_name = 'posts'
-    paginate_by = 5
+    paginate_by = 5  
+
+@login_required
+def toggle_favorite(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.user in post.favorites.all():
+        post.favorites.remove(request.user)
+    else:
+        post.favorites.add(request.user)
+    return redirect('blog-home')
+
+def about_view(request):
+    latest_posts = Post.objects.order_by('-date_posted')[:5]
+    announcements = Announcement.objects.all()[:5]
+    calendar_events = Event.objects.all()[:5]
+    
+    context = {
+        'latest_posts': latest_posts,
+        'announcements': announcements,
+        'calendar_events': calendar_events,
+    }
+    
+    return render(request, 'blog/about.html', context)
+
+def delete_message(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        message_id = data.get('messageId')
+        return JsonResponse({"status": "success", "message": "Message deleted."})
+    return JsonResponse({"status": "error", "message": "Invalid request."})
+
+def chat_response(request):
+    if request.method == "POST":
+        user_message = request.POST.get("message")
+        bot_response = random.choice(AUTO_RESPONSES)
+        return JsonResponse({"user_message": user_message, "bot_response": bot_response})
